abeb4af848b9110fb7b620d3068be9605d191a65
rh616 <37759720+rh616@users.noreply.github.com>
2018-11-09 17:48:52 +0800

    Update message.js

--- liuyan/js/message.js
+++ liuyan/js/message.js
@@ -2,8 +2,8 @@
   var model = {
 
     init: function(){
-      var APP_ID = 'TsDnap9SEXjSvGSowP7gXXJC-gzGzoHsz'
-      var APP_KEY = 'rGye31p12mM3wFpNRn9RADu9'
+      var APP_ID = 'cS2Juz1M20CBXR9r24GS37MS-gzGzoHsz'
+      var APP_KEY = 'luxNjG7idyAXIKDHIQJngCAg'
       AV.init({ appId: APP_ID, appKey: APP_KEY })
     },
     fetch: function(){ 
@@ -24,56 +24,4 @@
   var view = document.querySelector('section.message')
 
 
-  var controller = {
-    view: null,
-    model: null,
-    messageList: null,
-    init: function(view, model){
-      this.view = view
-      this.model = model
-
-      this.messageList = view.querySelector('#messageList')
-      this.form = view.querySelector('form')
-      this.model.init()
-      this.loadMessages()
-      this.bindEvents()
-    },
-    loadMessages: function(){
-      this.model.fetch().then(
-        (messages) => {
-          let array = messages.map((item)=> item.attributes )
-          array.forEach((item)=>{
-            let li = document.createElement('li')
-            li.innerText = `${item.name}: ${item.content}`
-            this.messageList.appendChild(li)
-          })
-        } 
-      )
-    },
-    bindEvents: function(){
-      this.form.addEventListener('submit', function(e){
-        e.preventDefault()
-        this.saveMessage()
-      })
-    },
-    saveMessage: function(){
-      let myForm = this.form
-      let content = myForm.querySelector('input[name=content]').value
-      let name = myForm.querySelector('input[name=name]').value
-      this.model.save(name, content).then(function(object) {
-        let li = document.createElement('li')
-        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
-        let messageList = document.querySelector('#messageList')
-        messageList.appendChild(li)
-        myForm.querySelector('input[name=content]').value = ''
-        console.log(object)
-      })
-    }
-
-  }
-
-  controller.init(view, model)
-
-
-}.call()
-
+  var control
