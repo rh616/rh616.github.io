ece3308df9e418e4be4a5426667ca29256c84375
rh616 <37759720+rh616@users.noreply.github.com>
2018-11-16 16:43:43 +0800

    Update webpack.config.js

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-
 module.exports = {
   entry: './src/js/app.js',
   output: {
@@ -23,14 +22,14 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [{
-          loader: "style-loader" // creates style nodes from JS strings
+          loader: "style-loader"
         }, {
-          loader: "css-loader", // translates CSS into CommonJS
+          loader: "css-loader",
           options: { importLoaders: 1 }
         }, {
           loader: 'postcss-loader'
         },{
-          loader: "sass-loader" // compiles Sass to CSS
+          loader: "sass-loader"
         }]
       }
     ]
